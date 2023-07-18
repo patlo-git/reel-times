979d9917d4d4acb9239bb155b20eb95860f2ce71
patlo-git <56564279+patlo-git@users.noreply.github.com>
2023-07-18 15:02:45 -0700

    Edit Theater to render movies by theater

--- src/components/Theater.tsx
+++ src/components/Theater.tsx
@@ -6,48 +6,81 @@ interface Props {
   movies: TheaterProps[];
 }
 
-const Theater: React.FC<Props> = ({ movies }) => {
-  const moviesSubset: MovieProps[] = movies.map(
-    ({ tmsId, title, genres, ratings, runTime, showtimes }) => ({
-      tmsId,
-      title,
-      genres,
-      ratings,
-      runTime,
-      showtimes,
-    })
-  );
+interface MoviesByTheater {
+  [theaterId: string]: MovieProps[];
+}
 
-  console.log("moviesSubset: ", moviesSubset);
+const Theater: React.FC<Props> = ({ movies }) => {
+  const moviesByTheater: MoviesByTheater = {};
+
+  movies.forEach(({ showtimes, ...rest }) => {
+    showtimes.forEach((showtime) => {
+      const theaterId = showtime.theatre.id;
+      const { tmsId, ...movieData } = rest;
+      const existingMovie = moviesByTheater[theaterId]?.find(
+        (movie) => movie.tmsId === tmsId
+      );
+
+      if (existingMovie) {
+        existingMovie.showtimes.push(showtime);
+      } else {
+        const anyExistingMoviesAtTheater = moviesByTheater[theaterId];
+
+        const newMovie = {
+          tmsId,
+          ...movieData,
+          showtimes: [showtime],
+        };
+
+        moviesByTheater[theaterId] = [
+          ...(anyExistingMoviesAtTheater || []),
+          newMovie,
+        ];
+      }
+    });
+  });
+
   return (
     <div className="theater-container">
-      <div>
-        <div className="theater-name-wrapper">
-          <h1>Theater</h1>
-        </div>
-        <div className="movies-container">
-          <div className="movies-wrapper">
-            <>
-              {moviesSubset.map((movie) => {
-                const { tmsId, title, genres, ratings, runTime, showtimes } =
-                  movie;
+      {Object.entries(moviesByTheater).map(([theaterId, movies]) => {
+        const theaterName = movies[0].showtimes[0].theatre.name;
 
-                return (
-                  <Movie
-                    key={tmsId}
-                    tmsId={tmsId}
-                    title={title}
-                    genres={genres}
-                    ratings={ratings}
-                    runTime={runTime}
-                    showtimes={showtimes}
-                  />
-                );
-              })}
-            </>
+        return (
+          <div key={theaterId}>
+            <div className="theater-name-wrapper">
+              <h1>{theaterName}</h1>
+            </div>
+            <div className="movies-container">
+              <div className="movies-wrapper">
+                <>
+                  {movies.map((movie) => {
+                    const {
+                      tmsId,
+                      title,
+                      genres,
+                      ratings,
+                      runTime,
+                      showtimes,
+                    } = movie;
+
+                    return (
+                      <Movie
+                        key={tmsId}
+                        tmsId={tmsId}
+                        title={title}
+                        genres={genres}
+                        ratings={ratings}
+                        runTime={runTime}
+                        showtimes={showtimes}
+                      />
+                    );
+                  })}
+                </>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
