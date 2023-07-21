8e62274d47d9697eb156e40a3148ab41597b277c
patlo-git <56564279+patlo-git@users.noreply.github.com>
2023-07-21 13:01:37 -0700

    Set and render readonly default date and city

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,20 +1,37 @@
+import { useState } from "react";
+
 const FilterMenu = () => {
-  const location = "Santa Barbara, CA";
+  const [location, setLocation] = useState("Santa Barbara, CA");
+  const todayDate = new Date().toISOString().slice(0, 10);
 
   return (
     <div className="search-params">
       <form>
         <div>
           <label htmlFor="location">
-            Location{" "}
-            <input id="location" value={location} placeholder="Location" />
+            Location
+            <input
+              id="location"
+              value={location}
+              placeholder="Location"
+              // Disabled since we're currently only getting data for one city
+              // onChange={(e) => setLocation(e.target.value)}
+              readOnly={true}
+            />
           </label>
-          <button>Submit</button>
+          <button disabled={true}>Submit</button>
         </div>
 
         <div>
           <label htmlFor="date">
-            Date <input id="date" type="date" name="date" />
+            Date{" "}
+            <input
+              id="date"
+              type="date"
+              name="date"
+              value={todayDate}
+              readOnly={true}
+            />
           </label>
         </div>
         <div>
